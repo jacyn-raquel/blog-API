5d30265a192c94d42f5412bd250fcb329437aec3
jacyn-raquel <jacynraquel@gmail.com>
2025-01-04 07:46:40 +0800

    Added image property to postSchema

--- models/Post.js
+++ models/Post.js
@@ -21,6 +21,17 @@ const postSchema = new mongoose.Schema({
 		type: String,
 		required: [true, "The author must be shown to know what user made the post"]
 	},
+	image: {
+       type: String,
+       required: [false], // Image is optional
+       validate: {
+           validator: function(value) {
+               // Optional validation for URL format
+               return /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|webp))$/i.test(value);
+           },
+           message: props => `${props.value} is not a valid image URL.`
+       }
+    },
 	datePostCreated: {
 		type: Date,
 		default: Date.now()
